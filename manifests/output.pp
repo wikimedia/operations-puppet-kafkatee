99dc792fcb989ab18af6321c0be1e603780daa7f
Ottomata <aotto@wikimedia.org>
2018-02-21 14:04:12 -0500

    Refactor kafkatee module to support multi instance
    Tested in Cloud VPS.  This will require manually removing single
    instance configs on oxygen and rhenium.
    
    Bug: T187890
    Change-Id: I002842b978aebaebe390bf210679c2ae985a059f

--- manifests/output.pp
+++ manifests/output.pp
@@ -2,6 +2,8 @@
 # Configures a kafkatee output.
 #
 # == Parameters
+# $instance_name    - Name of kafkatee::instance.
+#
 # $destination      - Where this output will be sent.  If $type is
 #                     'file', then this should be a file path.  Otherwise
 #                     it should be a process that receives input from stdin.
@@ -11,15 +13,16 @@
 #                     e.g. 1 means 100%, 1/10 means 10%, etc.
 #
 define kafkatee::output(
+    $instance_name,
     $destination,
     $type           = 'file',
     $sample         = 1,
     $ensure         = 'present',
 )
 {
-    file { "/etc/kafkatee.d/output.${title}.conf":
+    file { "/etc/kafkatee/${instance_name}.outputs/output.${title}.conf":
         ensure  => $ensure,
         content => template('kafkatee/output.conf.erb'),
-        notify  => Service['kafkatee'],
+        notify  => Service["kafkatee-${instance_name}"],
     }
 }
